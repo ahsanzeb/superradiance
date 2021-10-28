e72ec64a45b1163a7b0f6499bee6e81515e8b332
Ahsan Zeb <ahsan.zeb@hotmail.com>
2021-10-28 12:14:20 +0500

    using Hg1 and Hb1, built Hg1f and Hb1f; matrices in FULL hilbert space that can now be used to build the full hamiltonian. Other terms in the Ham for the rest of the system + the diagonal terms for the two fission molecules required.

--- src/hamiltonian.f
+++ src/hamiltonian.f
@@ -264,9 +264,9 @@
 	 ! co rotating term: a^- [total q terms]
 	 if(p>0) then
 	  i2 = i1 + q; 
-		H1r%coo1(i1+1:i2) = n0 + lrange;
-		H1r%coo2(i1+1:i2) = n0dn + lrange;	 ! n0dn: one less photon
-		H1r%coodat(i1+1:i2) = x;
+		Hg1%coo1(i1+1:i2) = n0 + lrange;
+		Hg1%coo2(i1+1:i2) = n0dn + lrange;	 ! n0dn: one less photon
+		Hg1%coodat(i1+1:i2) = x;
 		i1 = i2;
 	 endif
 		
@@ -277,6 +277,93 @@
 	Hb1%coo1(1:ntotsym) = (/ (i, i=1,ntotsym)/);
 	Hb1%coo2(1:ntotsym) = (/ (i, i=1,ntotsym)/);
 	Hb1%coodat(1:ntotsym) = 1.0d0; 
+
+
+
+
+	! using Hg1 and Hb1 blocks, build full space Hg1f and Hb1f
+	Hg1f%nnz = 6*Hg1%nnz; ! 3 blocks for first mol (each for a diagonal block of the second) and 3 blocks for second mol, etc.
+	if(allocated(Hg1f%coo1))deallocate(Hg1f%coo1)
+	if(allocated(Hg1f%coo2))deallocate(Hg1f%coo2)
+	if(allocated(Hg1f%coodat))deallocate(Hg1f%coodat)
+	allocate(Hg1f%coo1(Hg1f%nnz))
+	allocate(Hg1f%coo2(Hg1f%nnz))
+	allocate(Hg1f%coodat(Hg1f%nnz))
+
+	Hb1f%nnz = 6*Hb1%nnz;
+	if(allocated(Hb1f%coo1))deallocate(Hb1f%coo1)
+	if(allocated(Hb1f%coo2))deallocate(Hb1f%coo2)
+	if(allocated(Hb1f%coodat))deallocate(Hb1f%coodat)
+	allocate(Hb1f%coo1(Hb1f%nnz))
+	allocate(Hb1f%coo2(Hb1f%nnz))
+	allocate(Hb1f%coodat(Hb1f%nnz))
+
+
+
+	p1 = 0; q1=0;
+	do i1=1,3
+	 k1 = (i1-1)*ntotsym
+	 !l1 = k1 + ntotsym
+	 do j1=1,3
+	  m1 = (j1-1)*ntotsym
+	  !n1 = m1 + ntotsym
+	  do i2=1,3
+	   k2 = (i2-1)*ntotsym
+	   !l2 = k2 + ntotsym
+	   do j2=1,3
+	    m2 = (j2-1)*ntotsym
+	    !n2 = m2 + ntotsym
+
+			! molecule 1: |S1><G1| & diagonal in mol 2
+			if(i1==3 .and. j1==1 .and. i2==j2) then 
+			 p2 = p1 + ntotsym;
+	     Hg1f%coo1(p1+1:p2) = k1 + Hg1%coo1
+	     Hg1f%coo2(p1+1:p2) = m1 + Hg1%coo2
+	     Hg1f%coodat(p1+1:p2) = Hg1%coodat
+	     p1 = p2;
+			endif
+
+			! molecule 2: |S2><G2| & diagonal in mol 1
+			if(i2==3 .and. j2==1 .and. i1==j1) then 
+			 p2 = p1 + ntotsym;
+	     Hg1f%coo1(p1+1:p2) = k2 + Hg1%coo1
+	     Hg1f%coo2(p1+1:p2) = m2 + Hg1%coo2
+	     Hg1f%coodat(p1+1:p2) = Hg1%coodat
+	     p1 = p2;
+			endif
+
+
+			! molecule 1: |S1><T1| & diagonal in mol 2
+			if(i1==2 .and. j1==1 .and. i2==j2) then 
+			 q2 = q1 + ntotsym;
+	     Hg1f%coo1(q1+1:q2) = k1 + Hg1%coo1
+	     Hg1f%coo2(q1+1:q2) = m1 + Hg1%coo2
+	     Hg1f%coodat(q1+1:q2) = Hg1%coodat
+	     q1 = q2;
+			endif
+
+			! molecule 2: |S2><T2| & diagonal in mol 1
+			if(i2==2 .and. j2==1 .and. i1==j1) then 
+			 q2 = q1 + ntotsym;
+	     Hg1f%coo1(q1+1:q2) = k2 + Hg1%coo1
+	     Hg1f%coo2(q1+1:q2) = m2 + Hg1%coo2
+	     Hg1f%coodat(q1+1:q2) = Hg1%coodat
+	     q1 = q2;
+			endif
+
+
+
+	   end do
+	  end do
+	 end do
+	end do
+
+
+
+
+
+
+
 
 	return
 	end 	subroutine MakeH2
@@ -577,6 +664,153 @@
 	return
 	end subroutine coocsr 
 	!----------------------------------------------------------
+
+
+
+
+
+
+	subroutine MakeHhtc2(n,ijob)!, wr,delta,lambda,wv)
+	implicit none
+	integer, intent(in) :: n,ijob
+	!double precision, intent(in) :: wr, delta, lambda, wv
+
+	! local
+	double precision :: g, lamwv, w0, wcc, wv
+	integer :: i,n1,n2,n3,nnz, Hbnnzm
+
+	! parameters for this job
+	wr = param(ijob)%wr; ! omega_R
+	delta = param(ijob)%del; ! detuning = w0-w
+	lambda = param(ijob)%lam; ! for lambda_SOC
+
+	g = wr/dsqrt(dble(n));
+	lamwv = lambda;
+
+	! half values needed for diagonal terms:  
+	! *0.5 here for efficieny
+	! use wc instead of param(ijob)%wc
+	wcc = (wc)*0.5d0; ! Cavity
+	w0 = (wc+delta)*0.5d0; !Signlet
+	wv = (wc-param(ijob)%j)*0.5d0; ! triplet
+
+	!write(*,*)"wc, w0, wt = ", 2*wcc, 2*w0, 2*wv
+
+	! Hg has upper (a^+, counter-rotating terms) triangular
+	!      AND lower (a^-, co-rotating terms) triangular elements.
+	! no issues with having mixed upper/lower elements as long as they appear once in either upper or lower triangular.
+	! diagonal: Hdv are halved for matvec()
+
+	!..............................................
+	! combine all terms to make full Hamiltnian in coo
+	!..............................................
+
+
+
+	nnz = Hg%nnz + Hb%nnz + Hg%ntot; ! +Hg%ntot for size of diagonal term, Hv+Hd 
+
+
+!	write(*,*) "Hg%nnz, Hb%nnz, Hg%ntot, Hhtc%nnz = ", 
+!     . Hg%nnz, Hb%nnz, Hg%ntot, nnz
+	
+	Hhtc%nnz = nnz;
+	if(allocated(Hhtc%coo1))deallocate(Hhtc%coo1)
+	if(allocated(Hhtc%coo2))deallocate(Hhtc%coo2)
+	if(allocated(Hhtc%coodat))deallocate(Hhtc%coodat)
+	allocate(Hhtc%coo1(nnz))
+	allocate(Hhtc%coo2(nnz))
+	allocate(Hhtc%coodat(nnz))
+
+	if(allocated(Hdv)) deallocate(Hdv)
+	allocate(Hdv(Hg%ntot))  ! set Hg%ntot first
+
+	Hdv = wcc*Hc + w0*Hs +  wv*Ht; ! summ arrays, values.
+	! diagonal will be kept seperate
+
+!	write(*,'(1000i5)') Hg%coo1
+
+
+	! Hg first
+	n1 = 0;
+	n3 = Hg%nnz;
+	n2 = n1 + n3;
+!	write(*,*)"Hg: n1,n2,n3 = ", n1,n2,n3
+	Hhtc%coo1(n1+1:n2) = Hg%coo1(1:n3)
+	Hhtc%coo2(n1+1:n2) = Hg%coo2(1:n3)
+	Hhtc%coodat(n1+1:n2) = g * Hg%coodat(1:n3)
+
+
+
+	! Hb afterwards
+	n1 = n2;
+	n3 = Hb%nnz;
+	n2 = n1 + n3;
+!	write(*,*)"Hb: n1,n2,n3 = ", n1,n2,n3
+	Hhtc%coo1(n1+1:n2) = Hb%coo1(1:n3)
+	Hhtc%coo2(n1+1:n2) = Hb%coo2(1:n3)
+	Hhtc%coodat(n1+1:n2) = lamwv * Hb%coodat(1:n3)
+
+
+	! now diagonal terms
+	n1 = n2;
+	n3 = Hg%ntot;
+	n2 = n1 + n3;
+!	write(*,*)"Hd: n1,n2,n3 = ", n1,n2,n3
+	Hhtc%coo1(n1+1:n2) = (/ (i,i=1,n3) /)
+	Hhtc%coo2(n1+1:n2) = (/ (i,i=1,n3) /) 
+	Hhtc%coodat(n1+1:n2) = Hdv(1:n3) ! already halved for matvec(), see wc,w0,wt above.
+
+	!write(*,'(a,3x,10000f15.10)') 'Hdv = ',Hhtc%coodat(n1+1:n2)
+
+	!write(*,*) 'Hdv: n1, n2 = ',n1, n2 
+
+
+!	write(*,'(1000i5)') Hhtc%coo1
+!	write(*,'(1000i5)') Hhtc%coo2
+
+
+	!write(*,*)'============ htc =========='
+	
+	Hf%dense = .false.
+	Hf%ntot = Hg%ntot ! dimension of full hilbert space
+	Hf%nnz = Hhtc%nnz; 
+	if (Hf%ntot .le. nmaxddiag .and. ddiagOK) then 
+		! Hhtc in dense format and use direct diagonalisation
+		Hf%dense = .true.
+		n1 =Hf%ntot;
+		if(allocated(Hf%h))deallocate(Hf%h)
+		allocate(Hf%h(n1,n1))
+		call coo2dense(n1, n2, Hhtc%coo1(1:n2),
+     .      Hhtc%coo2(1:n2),Hhtc%coodat(1:n2), Hf%h)
+
+	 !write(*,*)'************* H *******************'
+	 !do i=1,n1
+	 ! write(*,'(1000f6.2)') Hf%h(i,:)
+	 !end do
+	 !write(*,*)'************************************'
+
+	else ! iterative diaognalisation
+		!..............................................
+		! change the format to CSR 
+		!..............................................
+		if(allocated(Hf%col))deallocate(Hf%col)
+		if(allocated(Hf%dat))deallocate(Hf%dat)
+		if(allocated(Hf%rowpntr))deallocate(Hf%rowpntr)
+		allocate(Hf%col(nnz))
+		allocate(Hf%dat(nnz))
+		allocate(Hf%rowpntr(Hf%ntot + 1))
+		call coocsr(Hf%ntot, Hf%nnz, 
+     .  Hhtc%coodat, Hhtc%coo1, Hhtc%coo2,  
+     .  Hf%dat, Hf%col, Hf%rowpntr)
+	endif
+
+	
+!	endif
+
+
+	return
+	end subroutine MakeHhtc2
+	!--------------------------------------------------
 
 
 
